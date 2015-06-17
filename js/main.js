8bb06fb0e2772be192b4c3255f35b6cf02639098
laurenfazah <lauren.fazah@hhcc.com>
2015-06-17 16:51:33 -0400

    counter updating according to position in video

--- js/main.js
+++ js/main.js
@@ -3,14 +3,12 @@ var obj = {
         pastDate:0,
         currentYear:new Date().getFullYear()
     }
+var compiledDate = obj.olympicDate + obj.pastDate;
+var olympics = new Date(compiledDate, 8-1, 10);
 
-// $(function countDown() {
-setInterval(function(){
-    var compiledDate = obj.olympicDate + obj.pastDate;
-    var olympics = new Date(compiledDate, 8-1, 10);
+$(function () {
     $('#countdown-banner').countdown({until: olympics, format: 'YOWDHMS'});
-    console.log("test", obj.pastDate);
-},1000);
+});
 
 $(function() {
     var iframe = $('#player1')[0];
@@ -45,34 +43,38 @@ $(function() {
     }
 
     function year (data, id) {
-        if (data.seconds > 3 && data.seconds < 4) {
+        if (data.seconds < 3) {
             // 2007 marker on timeline
             obj.pastDate = obj.currentYear - 2007;
-            console.log(obj.pastDate);
+            compiledDate = obj.olympicDate + obj.pastDate;
+            olympics = new Date(compiledDate, 8-1, 10);
+            $('#countdown-banner').countdown('destroy');
+            $('#countdown-banner').countdown({until: olympics, format: 'YOWDHMS'});
         };
         if (data.seconds > 6 && data.seconds < 7) {
             // 2010 marker on timeline
             obj.pastDate = obj.currentYear - 2010;
-            console.log(obj.pastDate);
+            compiledDate = obj.olympicDate + obj.pastDate;
+            olympics = new Date(compiledDate, 8-1, 10);
+            $('#countdown-banner').countdown('destroy');
+            $('#countdown-banner').countdown({until: olympics, format: 'YOWDHMS'});
         };
         if (data.seconds > 10 && data.seconds < 11) {
             // 2013 marker on timeline
             obj.pastDate = obj.currentYear - 2013;
-            console.log(obj.pastDate);
+            compiledDate = obj.olympicDate + obj.pastDate;
+            olympics = new Date(compiledDate, 8-1, 10);
+            $('#countdown-banner').countdown('destroy');
+            $('#countdown-banner').countdown({until: olympics, format: 'YOWDHMS'});
         };
         if (data.seconds > 14 && data.seconds < 15) {
             // 2015 marker on timeline
             obj.pastDate = obj.currentYear - 2015;
-            console.log(obj.pastDate);
+            compiledDate = obj.olympicDate + obj.pastDate;
+            olympics = new Date(compiledDate, 8-1, 10);
+            $('#countdown-banner').countdown('destroy');
+            $('#countdown-banner').countdown({until: olympics, format: 'YOWDHMS'});
         };
     }
 
 });
-
-// obj.watch('pastDate', function(id, oldval, newval){
-//     var compiledDate = obj.olympicDate + obj.pastDate;
-//     var olympics = new Date(compiledDate, 8-1, 10);
-//     $('#countdown-banner').countdown({until: olympics, format: 'YOWDHMS'});
-// })
-
-
--- todo.md
+++ todo.md
@@ -1,5 +1,5 @@
--get countdown working on js
--target points in video, set variable accordingly
--date for countdown changes according to date of variable
+<!-- -get countdown working on js -->
+<!-- -target points in video, set variable accordingly -->
+<!-- -date for countdown changes according to date of variable -->
 -create interactive timeline
 -variable changes based on where on timeline user clicks
